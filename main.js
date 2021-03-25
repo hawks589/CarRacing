0153080a5f5ab8d0bc81823092a5e817335b43f6
hawks589 <75762466+hawks589@users.noreply.github.com>
2021-03-25 15:38:44 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 canvas = document.getElementById("myCanvas");
 ctx = canvas.getContext("2d");
 
-
 car1_width = 120;
 car1_height = 75;
 car1_image = "Car1.png";
@@ -46,23 +45,32 @@ function uploadCar2()
     ctx.drawImage(car2_imgTag,car2_x, car2_y, car2_width, car2_height);
 }
 
-window.addEventListener("keydown", my_key_down); 
+window.addEventListener("keydown", key_Down);
+function key_Down() 
+{
+    keyPressed = e.keyCode;
+    console.log(keyPressed);
 
-function my_key_down(e)
- { keyPressed = e.keyCode; console.log(keyPressed); 
-    if (keyPressed=='38') 
-    { up(); 
-        console.log("up"); }
-  if (keyPressed=='40')
-   { down(); 
-    console.log("down"); }
-   if (keyPressed=='37')
-    { left(); 
-    console.log("left"); } 
-   if (keyPressed=='39')
-    { right(); 
-    console.log("right"); }
- 
+    if (keyPressed == '38')
+    {
+        car1_up();
+        console.log("Car1: Up arrow key")
+    }
+    if (keyPressed == '40')
+    {
+        car1_down();
+        console.log("Car1: Down arrow key")
+    }
+    if (keyPressed == '37')
+    {
+        car1_left();
+        console.log("Car1: Left arrow key")
+    }
+    if (keyPressed == '39')
+    {
+        car1_right();
+        console.log("Car1: Right arrow key")
+    }
     if (keyPressed == '85')
     {
         car2_up();
@@ -84,107 +92,4 @@ function my_key_down(e)
         car2_left();
         console.log("Car2: L alphabet key")
     }
-
-    if (car1_x>700) {
-        console.log("Car 1 (Green) has won the race! Congrats!");
-        document.getElementById("game_status").innerHTML="Car 1 has won the race! Congrats!";
-        document.getElementById("game_status").style.color="yellow";
-    }
-    if (car2_x>700) {
-        console.log("Car 2 (Pink) has won the race! Congrats!");
-        document.getElementById("game_status").innerHTML="Car 2 has won the race! Congrats!";
-        document.getElementById("game_status").style.color="yellow";
-    }
 }
-
-function up() {
-    if (car1_y >= 0) {
-        car1_y = car1_y - 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "hidden";
-    }
-}
-
-function down() {
-    if (car1_y <= 500) {
-        car1_y = car1_y + 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "hidden";
-    }
-}
-
-function left() {
-    if (car1_x >= 0) {
-        car1_x = car1_x - 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "visible";
-    }
-}
-
-function right() {
-    if (car1_x <= 700) {
-        car1_x = car1_x + 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "visible";
-    }
-}
-
-function car2_up()
-{
-    if (car2_y >= 0) {
-        car2_y = car2_y - 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "hidden";
-    }
-}
-
-function car2_down()
-{
-    if (car2_y <= 500) {
-        car2_y = car2_y + 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "hidden";
-    }
-}
-
-function car2_right() {
-    if (car2_x <= 700) {
-        car2_x = car2_x + 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = " visible";
-    }
-}
-
-function car2_left() {
-    if (car2_x >= 0) {
-        car2_x = car2_x - 10;
-        uploadBackground();
-        uploadCar1();
-        uploadCar2();
-        document.body.style.overflow = "visible";
-    }
-}
-function reset()
-{
-    window.location.reload();
-}
-
-
-
-
-
-
